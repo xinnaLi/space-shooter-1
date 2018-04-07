0db257de7ccf8825f1188360c5426b3d4b96fa5c
Colton Jackson <colton95@fastmail.com>
2018-04-07 18:52:53 -0400

    Commit #20

--- Assets/_Game/Scripts/DestroyByContact.cs
+++ Assets/_Game/Scripts/DestroyByContact.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour {
 
     public GameObject explosion;
-    public GameObject playerExplosion;
     public float scoreValue;
     
     private void OnCollisionEnter(Collision other)
     {
         Instantiate(explosion, transform.position, transform.rotation);
-        if (other.gameObject.tag == "Player")
-        {
-            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-        }
-        Destroy(other.gameObject);
         Destroy(this.gameObject);
     }
 }
